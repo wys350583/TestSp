a9a81bf26e8e648fd1c3960f4e3b06f3b7e4cba5
wyhazq <443265447@qq.com>
2015-12-23 21:12:13 +0800

    Initial Commit

---
+++ TestSp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSp
+//
+//  Created by 一鸿温 on 15/12/23.
+//  Copyright © 2015年 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestSp
+//
+//  Created by 一鸿温 on 15/12/23.
+//  Copyright © 2015年 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
